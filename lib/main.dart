9cc36d1a1b94659839ede7698678d71a7015f2bc
alonsorios <salditoalonso@gmail.com>
2019-07-08 21:05:32 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'loginemail.dart';
+
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Google Sign In',
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    ),
+  );
+}
